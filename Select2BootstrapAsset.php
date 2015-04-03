591c73693dba68b52e72f6a89224c02e7095f060
Vitaliy Syrchikov <maddoger@gmail.com>
2015-04-03 19:36:31 +0400

    New style for bootstrap using https://github.com/t0m/select2-bootstrap-css

--- Select2BootstrapAsset.php
+++ Select2BootstrapAsset.php
@@ -9,12 +9,12 @@ class Select2BootstrapAsset extends AssetBundle
     /**
      * @inheritdoc
      */
-    public $sourcePath = '@bower/select2';
+    public $sourcePath = '@bower/select2-bootstrap-css';
     /**
      * @inheritdoc
      */
     public $css = [
-        'select2-bootstrap.css'
+        'select2-bootstrap.min.css'
     ];
     /**
      * @inheritdoc
